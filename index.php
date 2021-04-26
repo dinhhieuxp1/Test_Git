c0136013726af36e1e83fe38f9e5550d9171a016
Nguyen Dinh Hieu <dinhhieuxp293@email.com>
2021-04-26 10:53:05 +0700

    edit index

--- index.php
+++ index.php
@@ -2,8 +2,5 @@
 // Th: conflict co ng sua code ma minh khong biet truoc
 // Th: conflict khi merge code sang branch khac
  echo "Xin Chao!Rat vui duoc gap ban.Ban bao nhieu tuoi";
- echo "Ban can minh giup gi nao?";
-
-
-
+ echo "So thich ban la gi?,ban thich doc sach chu";
 
